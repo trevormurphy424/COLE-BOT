37defd66e18c31d06d3f44c4fbb77295c537eaed
trevormurphy424 <83364510+trevormurphy424@users.noreply.github.com>
2025-10-28 16:23:01 -0600

    Refactor RX_Code.ino for Receiver mode only
    Removed unused joystick and button input handling for transmitter mode.

--- RX/RX_Code.ino
+++ RX/RX_Code.ino
@@ -29,15 +29,7 @@ void setup() {
 
   Serial.begin(115200);  //preferred transmission rate for Arduino UNO R4
 
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(joy1X, INPUT);
-  pinMode(joy1Y, INPUT);
-  pinMode(joy2X, INPUT);
-  pinMode(joy2Y, INPUT);
-
-  // WifiSerial.begin("group88", "superSecurePassword", WifiPortType::Transmitter);
   WifiSerial.begin("group88", "superSecurePassword", WifiPortType::Receiver);
-  // WifiSerial.begin("group88", "superSecurePassword", WifiPortType::Emulator); // one board to rule them all debugging
 }
 
 void loop() {
@@ -45,34 +37,14 @@ void loop() {
   if (WifiSerial.getPortType() == WifiPortType::Transmitter || WifiSerial.getPortType() == WifiPortType::Emulator) {
     WifiSerial.autoReconnect();//try and connect
 
-    data.joystick1X = analogRead(joy1X);
-    data.joystick1Y = analogRead(joy1Y);
-    data.joystick2X = analogRead(joy2X);
-    data.joystick2Y = analogRead(joy2Y);
-    buttonState = digitalRead(buttonPin);
-
-    if(!buttonState && buttonState != prevState) {
-      data.button = !data.button;
-    }
-
     //Tx stuff below
-    Serial.println("Sending: " );
-    Serial.print("Button: ");
-    Serial.println(data.button);
-    Serial.print("Joystick 1: (");
-    Serial.print(data.joystick1X);
-    Serial.print(", ");
-    Serial.print(data.joystick1Y);
-    Serial.println(")");
+    Serial.println("Incorrect mode selected. Please select receive.");
     //Tx stuff above
-
 
     if (!WifiSerial.sendData(data))//check and see if connection is established and data is sent
       Serial.println("Wifi Send Problem");//oh no it didn't send --> it iwll try and re-connect at the start of the loop
 
   }
-
-
   
   if ((WifiSerial.getPortType() == WifiPortType::Receiver || WifiSerial.getPortType() == WifiPortType::Emulator) && WifiSerial.checkForData()) {
 
