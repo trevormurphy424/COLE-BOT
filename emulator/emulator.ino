8325ca54677b1866cfa7e39d21375f4e104b09c7
trevormurphy424 <83364510+trevormurphy424@users.noreply.github.com>
2025-10-30 14:36:53 -0600

    Implement servo control and update motor settings
    Added servo control for arm and claw, updated motor pin assignments, and modified joystick handling for speed and direction.

--- emulator/emulator.ino
+++ emulator/emulator.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include "WifiPort2.h"
 #include "motor.h"
 
@@ -17,14 +18,21 @@ struct DataPacket {
 
 WifiPort<DataPacket> WifiSerial;
 
-dcMotor LMotor(3, 4, 5);
-dcMotor RMotor(7, 8, 6);
+dcMotor LMotor(3, 4, 9);
+dcMotor RMotor(7, 8, 10);
+Servo clawServo;
+Servo armServo;
 
 int xAxis, yAxis;
 int leftSpeed, rightSpeed;
 int leftPower, rightPower;
+int armDelta, clawDelta;
+int armPos, clawPos;
 
 // pin definitions
+const short unsigned CLAW_SERVO_PIN = 6;
+const short unsigned ARM_SERVO_PIN = 5;
+
 int joy1X = A0, joy1Y = A1, joy2X = A2, joy2Y = A3;
 int buttonPin = 2;
 
@@ -40,6 +48,14 @@ void setup() {
   pinMode(joy1Y, INPUT);
   pinMode(joy2X, INPUT);
   pinMode(joy2Y, INPUT);
+
+  clawServo.attach(CLAW_SERVO_PIN);
+  armServo.attach(ARM_SERVO_PIN);
+  
+  clawServo.write(10);
+  clawPos = 10;
+  armServo.write(70);
+  armPos = 70;
 
   WifiSerial.begin("group88", "superSecurePassword", WifiPortType::Emulator); // one board to rule them all debugging
 }
@@ -90,34 +106,45 @@ void loop() {
     Serial.print(data.joystick1Y);
     Serial.println(")");
 
-    xAxis = analogRead(data.joystick1X) - 512;
-    yAxis = analogRead(data.joystick1Y) - 512;
-
-    int leftPower = y + x;
-    int rightPower = y - x;
-
-    if(button) {
-      leftSpeed = map(abs(leftPower), 0, 512, 0, 255);
-      rightSpeed = map(abs(rightPower), 0, 512, 0, 255);
-    } else {
-      leftSpeed = map(abs(leftPower), 0, 512, 0, 127);
-      rightSpeed = map(abs(rightPower), 0, 512, 0, 127);
-    }
-
-    if(leftPower >= 0) {
+    xAxis = data.joystick1X - 512;
+    yAxis = data.joystick1Y - 512;
+  
+    int L = xAxis + yAxis;
+    int R = xAxis - yAxis;
+  
+    L = map(L, -1023, 1023, -255, 255);
+    R = map(R, -1023, 1023, -255, 255);
+  
+    LMotor.setSpeed(abs(L));
+    RMotor.setSpeed(abs(R));
+  
+    if(L >= 0) {
       LMotor.setDirection(true);
     } else {
       LMotor.setDirection(false);
     }
-
-    if(rightPower >= 0) {
+  
+    if(R >= 0) {
       RMotor.setDirection(false);
     } else {
       RMotor.setDirection(true);
     }
 
-    LMotor.setSpeed(leftSpeed);
-    RMotor.setSpeed(rightSpeed);
+    armDelta = map(data.joystick2X, 0, 1023, -6, 6);
+    clawDelta = map(data.joystick2Y, 0, 1023, -6, 6);
+
+    Serial.print(armDelta);
+    Serial.print(" ");
+    Serial.println(clawDelta);
+
+    if(((armPos + armDelta) > 20) && ((armPos + armDelta) < 110)) {
+      armPos += armDelta;
+      armServo.write(armPos);
+    }
+    if(((clawPos + clawDelta) > 0) && ((clawPos + clawDelta) < 45)) {
+      clawPos += clawDelta;
+      clawServo.write(clawPos);
+    }
     //all RX stuff above
 
   }
