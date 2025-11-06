fa3a4c80f58cb6045287d48264d6ad87c1e54086
trevormurphy424 <83364510+trevormurphy424@users.noreply.github.com>
2025-11-06 12:16:36 -0700

    Refactor TX.ino for clarity and organization
    Refactor code for clarity and organization, including improved comments and initialization of variables.

--- TX/TX.ino
+++ TX/TX.ino
@@ -1,7 +1,6 @@
 #include "WifiPort2.h"
 
-// A structure, similar to our servo and stepper motors, but this one conatins variables to be transmitted
-// Any variable you want to transmit/recieve must be initalized in the DataPacket structure
+// data packet for WiFi transmission
 struct DataPacket {
 
   bool button = false;
@@ -12,76 +11,87 @@ struct DataPacket {
 
 } data;
 
-//gloabl vars are outside datapacket
-
+// initialize WiFi interface
 WifiPort<DataPacket> WifiSerial;
 
-int buttonPin = 2;
-int joy1X = A0;
-int joy1Y = A1;
-int joy2X = A2;
-int joy2Y = A3;
-bool buttonState;
-bool prevState;
+// debug
+const bool debug = false;
+
+// pins
+int buttonPin = 2,
+    joy1X = A0,
+    joy1Y = A1,
+    joy2X = A2,
+    joy2Y = A3;
+
+// button state variables
+bool  buttonState,
+      prevState;
 
 void setup() {
   //DONT USE PIN13 FOR ANY SENSOR OR ACTUATORS
 
   Serial.begin(115200);  //preferred transmission rate for Arduino UNO R4
 
+  // define pinMode for all inputs
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(joy1X, INPUT);
   pinMode(joy1Y, INPUT);
   pinMode(joy2X, INPUT);
   pinMode(joy2Y, INPUT);
 
+  // begin transmit on WiFi network 
   WifiSerial.begin("group88", "superSecurePassword", WifiPortType::Transmitter);
 }
 
 void loop() {
 
   if (WifiSerial.getPortType() == WifiPortType::Transmitter || WifiSerial.getPortType() == WifiPortType::Emulator) {
-    WifiSerial.autoReconnect();//try and connect
+    //try and connect
+    WifiSerial.autoReconnect();
 
+    // get values from all input devices
     data.joystick1X = analogRead(joy1X);
     data.joystick1Y = analogRead(joy1Y);
     data.joystick2X = analogRead(joy2X);
     data.joystick2Y = analogRead(joy2Y);
     buttonState = digitalRead(buttonPin);
 
+    // true toggle logic for button
     if(!buttonState && buttonState != prevState) {
       data.button = !data.button;
     }
 
     prevState = buttonState;
 
-    //Tx stuff below
-    Serial.println("Sending: " );
-    Serial.print("Button: ");
-    Serial.println(data.button);
-    Serial.print("Joystick 1: (");
-    Serial.print(data.joystick1X);
-    Serial.print(", ");
-    Serial.print(data.joystick1Y);
-    Serial.println(")");
-    //Tx stuff above
+    // TX debug
+    if(debug){
+      Serial.println("Sending: " );
+      Serial.print("Button: ");
+      Serial.println(data.button);
+      Serial.print("Joystick 1: (");
+      Serial.print(data.joystick1X);
+      Serial.print(", ");
+      Serial.print(data.joystick1Y);
+      Serial.println(")");
+    }
 
-
-    if (!WifiSerial.sendData(data))//check and see if connection is established and data is sent
-      Serial.println("Wifi Send Problem");//oh no it didn't send --> it iwll try and re-connect at the start of the loop
+    //check and see if connection is established and data is sent
+    if (!WifiSerial.sendData(data))
+      Serial.println("Wifi Send Problem");
 
   }
   
   if ((WifiSerial.getPortType() == WifiPortType::Receiver || WifiSerial.getPortType() == WifiPortType::Emulator) && WifiSerial.checkForData()) {
 
-    data = WifiSerial.getData();//received and unpack data structure
+    //received and unpack data structure
+    data = WifiSerial.getData();
 
-    //all Rx stuff below
+    // incorrect mode error
     Serial.println("Incorrect mode selected. Please change to transmit.");
     delay(1000);
-    //all RX stuff above
 
   }
 
-  delay(10); // update delay after you get it working to be a smaller number like 10ms to account for WiFi transmission overhead
+  delay(10); // WiFi transmission overhead delay
 }
